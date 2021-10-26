44a142f7d3f829bd7fb62b43df0ef5f224bb101d
“hassanharoon86” <hassanharoon86@gmail.com>
2021-10-26 16:50:27 +0500

    Passwords regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  # validates :password, format: { with: /(?=.{8,})(?=.*[A-Z])(?=.*[[:^alnum:]])/}
+  validates :password, format: { with: /(?=.{8,})(?=.*[A-Z])(?=.*[[:^alnum:]])/}
   validates :role, presence: true
   validates :terms_of_service, acceptance: true
 end
