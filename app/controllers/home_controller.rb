7f964fc9be7739bcba545a02d5614438f5a64eda
“hassanharoon86” <hassanharoon86@gmail.com>
2021-10-27 15:22:04 +0500

    Removed test files and renamed controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
-    if user_signed_in?
-      if current_user.role == 'manager'
-        puts "I am manager"
-        redirect_to manager_index_path
-      else
-        redirect_to artist_index_path
-      end
-    end
+    # if user_signed_in?
+    #   if current_user.role == 'manager'
+    #     puts "I am manager"
+    #     redirect_to managers_path
+    #   else
+    #     redirect_to artists_path
+    #   end
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  # get 'auditions/new'
-  # get 'auditions/create'
-  # get 'audition/new'
-  # get 'audition/create'
-  # get 'auditions/new'
-  # get 'auditions/create'
-  # get 'artist/index'
-  # get 'manager/index'
-  # get 'home/index'
-
   root to: 'home#index'
 
   resources :artists
