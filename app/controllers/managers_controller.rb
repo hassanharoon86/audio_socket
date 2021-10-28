3bae6a114157cd6ca7e6f3a55eac922219fd8979
“hassanharoon86” <hassanharoon86@gmail.com>
2021-10-28 16:29:43 +0500

    PR

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,4 +1,3 @@
 class ManagersController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   validates :password, format: { with: /(?=.{8,})(?=.*[A-Z])(?=.*[[:^alnum:]])/ }
   validates :role, presence: true
   validates :terms_of_service, acceptance: true
