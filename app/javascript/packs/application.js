4a4a07e3a270e3c7e90372a32d978c3af0a6bca9
“hassanharoon86” <hassanharoon86@gmail.com>
2021-10-28 17:38:45 +0500

    PR Commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,29 +18,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("@nathanvda/cocoon")
+require("packs/audition_submission")
 
 import 'bootstrap'
-
-
-$(document).on('turbolinks:load', function () {
-  $("#hear-about").on('change', function() {
-    if(this.value == 'Other'){
-      $('#other-field').fadeIn();
-    }
-    else{
-      $('#other-field').fadeOut();
-    }
-  });
-});
-
-$(document).on('cocoon:after-insert', function(){
-  if($(".nested-fields").length == 4) {
-    $('#add-link').removeClass('d-flex').hide();
-  }
-});
-
-$(document).on('cocoon:after-remove', function(){
-  if($(".nested-fields").length < 4){
-    $('#add-link').addClass('d-flex').fadeIn();
-  }
-})
---
+++ app/javascript/packs/audition_submission.js
@@ -0,0 +1,22 @@
+$(document).on('turbolinks:load', function () {
+  $("#hear-about").on('change', function() {
+    if(this.value == 'Other'){
+      $('#other-field').fadeIn();
+    }
+    else{
+      $('#other-field').fadeOut();
+    }
+  });
+});
+
+$(document).on('cocoon:after-insert', function(){
+  if($(".nested-fields").length == 4) {
+    $('#add-link').removeClass('d-flex').hide();
+  }
+});
+
+$(document).on('cocoon:after-remove', function(){
+  if($(".nested-fields").length < 4){
+    $('#add-link').addClass('d-flex').fadeIn();
+  }
+})
