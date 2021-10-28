ff1cc88f9901a6de0c8e4208200953babd9fdbba
“hassanharoon86” <hassanharoon86@gmail.com>
2021-10-28 10:31:14 +0500

    PR Review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   enum role: [:artist, :manager]
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
