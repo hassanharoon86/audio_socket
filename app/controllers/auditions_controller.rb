f5886964d3d48ede3f2e063405d442fe6facbf5a
“hassanharoon86” <hassanharoon86@gmail.com>
2021-10-29 11:42:00 +0500

    Resolved first index empty issue in genres

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -5,9 +5,7 @@ class AuditionsController < ApplicationController
   end
 
   def create
-    byebug
     @audition = Audition.new(audition_params)
-    byebug
     if @audition.save
       redirect_to root_path
     end
@@ -15,7 +13,6 @@ class AuditionsController < ApplicationController
 
   private
   def audition_params
-    byebug
     params.require(:audition).permit(:first_name, :last_name, :email, :artist_name, :hear_about, :additional_info, :other_source, links_attributes: [:id, :link, :_destroy], genres: [])
   end
 end
--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,4 +1,6 @@
 class Audition < ApplicationRecord
+  before_validation :remove_empty_genre
+
   GENRES = ['Acapella', 'Acid', 'Jazz', 'Acoustic', 'Acid', 'Acid Punk', 'Alternative', 'Hip Hop', 'Ambient',
     'Avantgarde', 'Bass', 'Blues', 'Cabaret', 'Celtic Chamber', 'Chanson', 'Chorus', 'Christian Rap', 'Cinematic', 'Classical',
     'Classic Rock', 'Club', 'Comedy', 'Country', 'Cult' ]
@@ -8,9 +10,13 @@ class Audition < ApplicationRecord
   validates :first_name, presence: true ,length: { maximum: 30 },format: { with: /\A[a-zA-Z]+\z/, message: "No special characters allowed" }
   validates :last_name, presence: true ,length: { maximum: 30 },format: { with: /\A[a-zA-Z]+\z/, message: "No special characters allowed" }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :artist_name, presence: true
-  validates :genres, presence: true
+  validates :first_name, :last_name, :email, :artist_name, :links, :genres, :hear_about, presence: true
 
   has_many :links, dependent: :destroy
   accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: :true
+
+
+  def remove_empty_genre
+    genres.reject! { |g| g.empty? }
+  end
 end
