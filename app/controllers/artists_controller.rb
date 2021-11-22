8b6d6177974c356794fa7be1d7813e665516f3fb
“hassanharoon86” <hassanharoon86@gmail.com>
2021-11-22 13:34:10 +0500

    Code refactored

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,7 +10,6 @@ class ArtistsController < ApplicationController
   private
 
   def setup_profile
-    # byebug
     if !(current_user.artist_detail && current_user.artist_detail.valid?)
       redirect_to new_user_artist_detail_path(current_user), notice: 'You must setup your profile to continue'
     end
